2cc159c75e689aef99bf11ae1469a382037848b3
Kathleen Rauh <krauh2010@gmail.com>
2015-09-08 17:22:28 -0400

    Counting turns working.

--- sticks.py
+++ sticks.py
@@ -5,23 +5,34 @@ def player_turn(turn_count):
         stick_subtract()
     else:
         print("Please enter a number between 1 and 3 ")
-
-def stick_subtract(pile, sticks_removed):
-    turn_count = 0
-    if (turn_count % 2) == 0:
-
-
-def game_set_up(pile):
-    print("There are {} sticks on the board".format(pile))
-    turn_count = 0
-    if (turn_count % 2) == 0:
-
-
-    game_set_up()
+        player_turn()
     return sticks_removed
 
 
+def stick_subtract(pile, sticks_removed):
+    #total number of sticks (game_size)
+    #player's chosen number to remove (sticks_removed)
+    pile = pile - sticks_removed
+
+    return pile
+
+
+def game_set_up():
+# Begin to count turns in order to keep track of which player's turn it is.
+    game_size()
+    print("There are {} sticks on the board".format(pile))
+    turn_count = 1
+    if (turn_count % 2) == 0:
+        player = '1'
+    else:
+        player = '2'
+    player_turn(turn_count)
+    game_set_up()
+    return turn_count
+
+
 def game_size():
+# Begins the game by asking user for the number of sticks in the game.  Passed test.
     print("Welcome to the Game of Sticks!")
     pile = input("How many sticks are there on the table initially (10-100)? ")
     if 10 <= int(pile) <= 100:
@@ -38,8 +49,7 @@ def game_loop():
 
 
 def main():
-    game_size():
-    game_set_up(pile):
+    game_set_up()
     game_loop():
     is_game_over():
 
