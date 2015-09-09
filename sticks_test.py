e5dd3251cac6753da2d69239d7bd1a2fdfc7e3a7
Kathleen Rauh <krauh2010@gmail.com>
2015-09-08 23:56:40 -0400

    Further attempt at a test.

--- sticks_test.py
+++ sticks_test.py
@@ -4,17 +4,17 @@ from sticks import *
 class TestSticks(unittest.TestCase):
 
 
-def sticks_taken():
-    word = "integration"
-    self.assertEqual(display_word(word, [4]), "_ _ _ _ _ _ _ _ _ _ _")
-    self.assertEqual(display_word(word, ["z"]), "_ _ _ _ _ _ _ _ _ _ _")
-    self.assertEqual(display_word(word, ["0"]), "_ _ _ _ G _ _ _ _ _ _")
-    self.assertEqual(display_word(word, ["i"]), "I _ _ _ _ _ _ _ I _ _")
-    self.assertEqual(display_word(word, ["i", "g"]), "I _ _ _ G _ _ _ I _ _")
-    self.assertEqual(display_word(word, ["i", "n", "z"]), "I N _ _ _ _ _ _ I _ N")
+def sticks_taken(self):
+    pile = 20
+    self.assertEqual(player_turns(sticks_removed,, [4]), "_ _ _ _ _ _ _ _ _ _ _")
+    self.assertEqual(player_turns(sticks_removed, ["z"]), 
+    self.assertEqual(player_turns(sticks_removed, ["33"]),
+    self.assertEqual(player_turns(sticks_removed,
+    self.assertEqual(player_turns(sticks_removed,
+    self.assertEqual(player_turns(sticks_removed,
 
 
 def sticks_on_table():
 
 
-def 
+def
